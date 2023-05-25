79abb520a5010e62b2e72f18984e437521730814
Sahana1115 <134591144+Sahana1115@users.noreply.github.com>
2023-05-25 13:42:14 +0530

    Add files via upload

---
+++ M.D.F.SAHANA...py
@@ -0,0 +1,19 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================================================== RESTART: C:/Users/AL AZHAR/Desktop/M.D.F.SAHANA.py ==================================================
+Hello,World!
+>>> 
+==================================== RESTART: C:/Users/AL AZHAR/AppData/Local/Programs/Python/Python311/Sahana...py ====================================
+Hello,World!
+What is your name?Sahana
+Hello Sahana
+>>> age=15
+>>> print(age)
+15
+>>> weight=45
+>>> print(weight)
+45
+>>> height=154
+>>> print(height)
+154
